b6de98769daabec84b6f96c1c03f6abf66ee3fce
mrs <renshen.ma@meritco-group.com>
2026-07-25 15:42:59 +0800

    fix: finish upstream sync adaptation

--- codex-rs/app-server/src/request_processors.rs
+++ codex-rs/app-server/src/request_processors.rs
@@ -336,7 +336,6 @@ use codex_connectors::AppInfo;
 use codex_core::CodexThread;
 use codex_core::CodexThreadSettingsOverrides;
 use codex_core::ForkSnapshot;
-use codex_core::ForkThreadHistoryOptions;
 use codex_core::McpManager;
 use codex_core::NewThread;
 use codex_core::NotSubmittedReason;
