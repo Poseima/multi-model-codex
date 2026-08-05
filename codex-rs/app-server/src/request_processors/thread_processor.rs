68442c1c95e18467c6d1c0ea9845528c7491e638
mrs <renshen.ma@meritco-group.com>
2026-08-05 16:22:15 +0800

    fix: adapt fork after upstream sync

--- codex-rs/app-server/src/request_processors/thread_processor.rs
+++ codex-rs/app-server/src/request_processors/thread_processor.rs
@@ -1290,32 +1290,26 @@ impl ThreadRequestProcessor {
             ..
         } = listener_task_context
             .thread_manager
-            .start_thread_with_options(
-                StartThreadOptions {
-                    allow_provider_model_fallback,
-                    initial_history: match session_start_source
-                        .unwrap_or(codex_app_server_protocol::ThreadStartSource::Startup)
-                    {
-                        codex_app_server_protocol::ThreadStartSource::Startup => {
-                            InitialHistory::New
-                        }
-                        codex_app_server_protocol::ThreadStartSource::Clear => {
-                            InitialHistory::Cleared
-                        }
-                    },
-                    history_mode,
-                    thread_source,
-                    dynamic_tools,
-                    persist_extended_history,
-                    metrics_service_name: service_name,
-                    parent_trace: request_trace,
-                    environments: Some(environments),
-                    thread_extension_init,
-                    prompt_profile_override,
-                    client_mcp_extensions,
-                    ..StartThreadOptions::new(config)
+            .start_thread_with_options(StartThreadOptions {
+                allow_provider_model_fallback,
+                initial_history: match session_start_source
+                    .unwrap_or(codex_app_server_protocol::ThreadStartSource::Startup)
+                {
+                    codex_app_server_protocol::ThreadStartSource::Startup => InitialHistory::New,
+                    codex_app_server_protocol::ThreadStartSource::Clear => InitialHistory::Cleared,
                 },
-            )
+                history_mode,
+                thread_source,
+                dynamic_tools,
+                persist_extended_history,
+                metrics_service_name: service_name,
+                parent_trace: request_trace,
+                environments: Some(environments),
+                thread_extension_init,
+                prompt_profile_override,
+                client_mcp_extensions,
+                ..StartThreadOptions::new(config)
+            })
             .instrument(tracing::info_span!(
                 "app_server.thread_start.create_thread",
                 otel.name = "app_server.thread_start.create_thread",
